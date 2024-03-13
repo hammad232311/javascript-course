3f35cae9b688ce3407ab7a00253b4e1ce8a4137c
hammad232311 <148622223+hammad232311@users.noreply.github.com>
2024-03-13 22:11:33 +0000

    data types

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,27 @@
+const accountId = 14453;
+let accountEmail = "asadmoon021@gmail.com";
+var accountPassword = "12345";
+cityName = "karachi";
+
+// accountId = 2;
+
+accountEmail = "asadjee@gmail.com";
+accountPassword = "123456";
+cityName = "islamabad";
+let accountState;
+/*
+prefer not to use var
+because of issue in block scope and functional scope.
+*/
+
+
+console.log(accountId);
+
+
+console.table([accountId, accountEmail, accountPassword, cityName, accountState])
+
+
+
+
+
+
---
+++ 01_basics/02-dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS as newer version
+
+//alert(3 + 3) we are using node js not browser
+
+
+
+console.log(3 
+    +
+     3)//code relibility should be high
+ console.log(3 + 3);
+
+
+
+ let name = "Muhammad Asad"
+ let age = 18
+ let isLoggedIn = true
+
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ //boolean => true or false
+ //null
+ //undefined
+ //Symbol
+
+ console.log(typeof "Muhammad Asad");
+
+ console.log(typeof undefined);
+ console.log(typeof null);
