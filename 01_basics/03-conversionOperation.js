35a480caac41e1894570723790ef2fabc15cb61b
hammad232311 <148622223+hammad232311@users.noreply.github.com>
2024-03-14 11:51:33 +0000

    conversion of data types

---
+++ 01_basics/03-conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = 'asad'
+
+
+
+// let valueNumber = Number(score);
+// console.log(valueNumber);
+
+//"33" => 33
+// "33abc" = NaN
+// true => 1
+// false => false
+
+
+// let isLoggedIn = 0
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+
+
+let someNumber = ""
+let stringNumber = Boolean(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
+// true => 1
+// false => false
+// "" => false
+// "muhamamd" => true
+
+
+
+
+
