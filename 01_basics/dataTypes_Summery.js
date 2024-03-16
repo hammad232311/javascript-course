5831e1002e25e5a28696b4473c82d3fb8e06903e
hammad232311 <148622223+hammad232311@users.noreply.github.com>
2024-03-16 13:02:24 +0000

    dataTypes Summery

---
+++ 01_basics/dataTypes_Summery.js
@@ -0,0 +1,39 @@
+// primitive dataTypes
+
+// 7 Types : String , Number , Boolean , Null , Undefined , Symbol , BigIn  
+
+
+//  const Score = 100
+//  const scoreValue = 100.3
+
+
+// const isLoggedIn = false
+// const putSideTemp = null
+// let Email;
+
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// let newNum = 2125643245612346530002553n
+// console.log(newNum);
+
+// Referance (non-primitive dataTypes)
+
+// arrays , Objects , Functions
+
+
+const heros = ['Quaid E azam','allama iqmal' , 'liaqar ali khan']
+
+let myOBJ = {
+    Name = asad
+    age = 19
+}
+
+const myFunction = function(){
+    console.log('hello World');
+} 
+
+
