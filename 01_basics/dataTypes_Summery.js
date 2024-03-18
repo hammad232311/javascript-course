c8863fd802fc8c0ee30b1deedaf55bb1e289ea80
hammad232311 <148622223+hammad232311@users.noreply.github.com>
2024-03-18 16:42:28 +0000

    heaps and stack

--- 01_basics/dataTypes_Summery.js
+++ 01_basics/dataTypes_Summery.js
@@ -1,6 +1,6 @@
 // primitive dataTypes
 
-// 7 Types : String , Number , Boolean , Null , Undefined , Symbol , BigIn  
+// 7 Types : String , Number , Boolean , Null , Undefined , Symbol , BigInt   
 
 
 //  const Score = 100
@@ -25,15 +25,53 @@
 // arrays , Objects , Functions
 
 
-const heros = ['Quaid E azam','allama iqmal' , 'liaqar ali khan']
+// const heros = ['Quaid E azam','allama iqmal' , 'liaqar ali khan']
 
-let myOBJ = {
-    Name = asad
-    age = 19
-}
+// let myOBJ = {
+//     Name = asad
+//     age = 19
+// }
 
-const myFunction = function(){
-    console.log('hello World');
-} 
+// const myFunction = function(){
+//     console.log('hello World');
+// } 
+
+
+// https://tc39.es/ecma262/#sec-intro
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+// Stack Memory (Premitive Types(7 Types : String , Number , Boolean , Null , Undefined , Symbol , BigInt)),
+
+// let myYoutubeName = 'Muhammad asad'
+// let anotherName = myYoutubeName
+
+// anotherName = 'asadmoon021'
+
+// console.log(typeof myYoutubeName);
+// console.log(typeof anotherName);
+
+
+
+
+// Heap Memory (Non-Premitive Types(arrays , Objects , Functions))
+
+
+
+
+
+// let userOne = {
+//     Email : 'asadmoon021@gmail.com',
+//     password : '03122598728'
+// }
+
+// let userTwo = userOne
+
+// userTwo.Email = 'asadjee.gmail.com'
+
+// console.log(userOne);
+// console.log(userTwo);
 
 
