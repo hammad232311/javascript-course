98c38fd9e09655ca318516c348477d053cc99810
hammad232311 <148622223+hammad232311@users.noreply.github.com>
2024-03-18 17:30:53 +0000

    pending

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,25 @@
+const userName = 'Muhammadasad'
+const repoCount = 50
+
+
+// console.log(userName + repoCount + ' Value'); (Old way for Addition Or Concatination of Srings)
+
+// console.log(`Hello i am ${userName} and my rapoCount is ${repoCount}`);
+// Modern Way with many banifits
+
+
+// different Ways to declare strings
+
+// const gameName = "Muhammad asad"
+
+
+const gameName = new String('muhammadasad')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase())
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf(3));
+
+
