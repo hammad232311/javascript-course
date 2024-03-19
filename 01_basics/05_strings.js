a26e13fb0ee5d131f26741a0674425816dd628a8
hammad232311 <148622223+hammad232311@users.noreply.github.com>
2024-03-19 15:15:15 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,4 +1,4 @@
-const userName = 'Muhammadasad'
+// const userName = 'Muhammadasad'
 const repoCount = 50
 
 
@@ -13,13 +13,40 @@ const repoCount = 50
 // const gameName = "Muhammad asad"
 
 
-const gameName = new String('muhammadasad')
+// const gameName = new String ('muhammad-asad')
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
 
 // console.log(gameName.toUpperCase())
 
-console.log(gameName.charAt(3));
-console.log(gameName.indexOf(3));
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('h'));
 
+
+// const newName = gameName.substring(0,6)
+
+
+
+
+
+
+
+// const gameName = new String ('muhammad-asad')
+
+// const newName = gameName.indexOf('muhammad')
+
+// let userName = '       muhammad asad      '
+// console.log(userName);
+// console.log(userName.trim());
+
+
+// const url = 'https://hitesh.com/hitesh%20Choudhary'
+// console.log(url);
+// console.log(url.includes('hitesh'));
+// console.log(url.replace('%20' , '-'));
+
+
+let userName = new String ('Muhammad-asad-khan')
+
+console.log(userName.split('-'));
 
