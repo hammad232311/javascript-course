02b627e57478c54e4382dd23780e88ee986a7742
hammad232311 <148622223+hammad232311@users.noreply.github.com>
2024-03-22 16:07:12 +0000

    Math_and+Number

---
+++ 01_basics/06_Nums_and_Maths.js
@@ -0,0 +1,44 @@
+// const score = 1000
+// console.log(score);
+
+
+
+// const Balance = new Number(150.66);
+// console.log(Balance);
+
+// console.log(Balance.toString().length);
+// console.log(typeof Balance.toString());
+
+// console.log(Balance.toFixed(3));
+
+
+// const newValue = new Number(150.66);
+// console.log(newValue.toPrecision(3));
+
+
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); //inDian Currency)
+
+// ++++++++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+// console.log(Math);
+// console.log(Math.abs(-5)); //Converts negative value to positive value
+// console.log(Math.round(4.6)); // rounds the Number to the closest one
+// console.log(Math.ceil(4.1)); // always gives the upper number 
+// console.log(Math.floor(4.9)); // always gives the lowwer number 
+// console.log(Math.min(4,3,6,9)); // to find the lowest value in the array
+// console.log(Math.min(4,3,6,9)); // to find the lowest value in the array
+// console.log(Math.random()*10+1); // always gives random value between 0 to 1
+
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 1;
+const max = 6;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min ) // Dice Game
