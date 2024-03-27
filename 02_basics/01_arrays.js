2c0a11f6fdda5440dd5060303b96695ce5c71d6d
hammad232311 <148622223+hammad232311@users.noreply.github.com>
2024-03-27 17:17:58 +0000

    arrays in js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,40 @@
+// arrays
+
+const myArr = [1,2,3,4,5]
+
+const myArr1 = ["Quaid E Azam" , "Allama Iqbal"]
+
+const myArr2 = new Array(1,2,3,4,5,6,7,8,9,10)
+
+
+// array methods
+
+
+
+// myArr.push(6)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr.indexOf(5));
+// console.log(myArr.includes(5));
+
+
+
+// const myNewArr = myArr.join()
+// console.log(typeof myNewArr);
+ 
+// Slice , Splice
+
+console.log("A " , myArr);
+
+const myn1 = myArr.slice(1 , 3)
+console.log(myn1);
+
+console.log("B " , myArr);
+
+const mny2 = myArr.splice(1 , 3)
+
+console.log("C " , myArr);
+console.log(mny2);
+
+
